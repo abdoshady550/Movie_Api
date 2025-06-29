b2b5ed6f7093de80ded939fbf7451eb28fe5421a
Abdelrahman <abdoshady550@gmail.com>
2025-06-29 04:44:42 +0300

    Add genre and movie management features
    - Updated `.gitignore` to ignore migration files and IDE configs.
    - Added `GenraController` and `MovieController` for handling genre and movie operations.
    - Modified `AppDbContext` to include `DbSet` for genres and movies.
    - Created `GenraConfigration` and `MovieConfigration` classes for entity configuration and seeding.
    - Added migration files for initial database schema setup.
    - Introduced `GenraDto` and `MovieDto` for data transfer.
    - Updated `Genra` and `Movie` entity classes with navigation properties.
    - Updated project file to target .NET 8.0 and include necessary packages.
    - Modified `Program.cs` to configure services for the application.
    - Added HTTP file for sample requests.

---
+++ Data/Configrations/GenraConfigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Movie_Api.Model.Eintites;
+
+namespace Movie_Api.Data.Configrations
+{
+    public class GenraConfigration : IEntityTypeConfiguration<Genra>
+    {
+        public void Configure(EntityTypeBuilder<Genra> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id);
+
+            builder.Property(e => e.Name)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(50)
+                .IsRequired();
+            builder.HasData(SeedGenres());
+        }
+        public static List<Genra> SeedGenres()
+        {
+            return new List<Genra>
+        {
+            new Genra { Id = 1, Name = "Action" },
+            new Genra { Id = 2, Name = "Comedy" },
+            new Genra { Id = 3, Name = "Drama" },
+            new Genra { Id = 4, Name = "Horror" },
+            new Genra { Id = 5, Name = "Sci-Fi" },
+            new Genra { Id = 6, Name = "Romance" }
+        };
+        }
+    }
+}
