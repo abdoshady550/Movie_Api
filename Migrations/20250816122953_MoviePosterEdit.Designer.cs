95107df576be818af6d355f07e4f518548f4bad9
Abdelrahman <abdoshady550@gmail.com>
2025-08-16 15:46:16 +0300

    Add MoviePosterEdit migration for Movies table
    Introduce a new migration to add a `PosterUrl` column
    to the `Movies` table, allowing storage of movie poster
    URLs. Update existing records to set `PosterUrl` to
    null and modify the model snapshot to reflect these
    changes in the Entity Framework Core context.

--- Migrations/20250816122953_MoviePosterEdit.Designer.cs
+++ Migrations/20250816122953_MoviePosterEdit.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movie_Api.Data;
 
@@ -10,9 +11,11 @@ using Movie_Api.Data;
 namespace Movie_Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250816122953_MoviePosterEdit")]
+    partial class MoviePosterEdit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250816122953_MoviePosterEdit.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movie_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoviePosterEdit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PosterUrl",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PosterUrl",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PosterUrl",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PosterUrl",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "PosterUrl",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "PosterUrl",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PosterUrl",
+                table: "Movies");
+        }
+    }
+}
